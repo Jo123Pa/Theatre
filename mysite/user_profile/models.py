cfb7b3659652c3b0ceeb7c3a451c46f1c90ed88f
Jonas Paliauka <jonaspa.vilnius@gmail.com>
2022-07-11 21:33:44 +0300

    Created theater and user_profile apps

--- mysite/user_profile/models.py
+++ mysite/user_profile/models.py
@@ -1,3 +1,27 @@
+from tabnanny import verbose
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        verbose_name=_('user'),
+        on_delete=models.CASCADE,
+        related_name='profile',
+    )
+    picture = models.ImageField(
+        _("picture"),
+        upload_to='user_profile/img/',
+        default ='user_profile/img/default.png',
+    )
+
+    def __str__(self):
+        return f'{str(self.user)} {_("profile")}'
+
+    class Meta:
+        verbose_name = _("user profile")
+        verbose_name_plural = _("user profiles")
+    
